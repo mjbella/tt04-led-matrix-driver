cf9889973aeb85796bd28f3552eddba5081135b6
Michael Bella <michael.j.bella@gmail.com>
2023-09-05 21:46:13 -0700

    Minor change

--- src/test.py
+++ src/test.py
@@ -2,8 +2,6 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
-
 async def reset_it(dut):
     # reset
     dut._log.info("reset")
